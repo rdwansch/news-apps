fa84aaca7d61a7b9527dfc019610a2c44f0234c2
ujklm23 <ridwanrasyid345@gmail.com>
2023-07-19 21:28:50 +0700

    add: react-navigation

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button, Text, View } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" options={{ title: 'NewsApp' }} component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button title="About" onPress={() => navigation.navigate('About')} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function AboutScreen({ navigation }) {
+  return (
+    <View>
+      <Text>About Screen</Text>
+      <Button title="Home" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+}
